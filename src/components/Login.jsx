a33aa5b7c115ce210f53c25530ace944cc5c1211
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-22 13:13:16 +0300

    Changing the markup based on the login state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -26,42 +26,54 @@ const Login = () => {
 	}    
 
     return (
-		<section>
-			<p
-				ref={errRef}
-				className={errMsg ? 'errmsg' : 'offscreen'}
-				aria-live='assertive'
-			>
-				{errMsg}
-			</p>
-			<h1>Sign In</h1>
-			<form onSubmit={handleSubmit}>
-				<label htmlFor='username'>Username:</label>
-				<input
-					type='text'
-					id='username'
-					ref={userRef}
-					autoComplete='off'
-					onChange={(e) => setUser(e.target.value)}
-					value={user}
-					required
-				/>
-				<label htmlFor='password'>Password:</label>
-				<input
-					type='password'
-					id='password'
-					onChange={(e) => setPwd(e.target.value)}
-					value={pwd}
-					required
-				/>
-				<button>Sign In</button>
-				<p>
-					Need an account?
+		<>
+			{success ? (
+				<section>
+					<h1>You are logged in</h1>
 					<br />
-					<span className='line'>Sign Up</span>
-				</p>
-			</form>
-		</section>
+					<p>
+						<a href='#'>Go Home</a>
+					</p>
+				</section>
+			) : (
+				<section>
+					<p
+						ref={errRef}
+						className={errMsg ? 'errmsg' : 'offscreen'}
+						aria-live='assertive'
+					>
+						{errMsg}
+					</p>
+					<h1>Sign In</h1>
+					<form onSubmit={handleSubmit}>
+						<label htmlFor='username'>Username:</label>
+						<input
+							type='text'
+							id='username'
+							ref={userRef}
+							autoComplete='off'
+							onChange={(e) => setUser(e.target.value)}
+							value={user}
+							required
+						/>
+						<label htmlFor='password'>Password:</label>
+						<input
+							type='password'
+							id='password'
+							onChange={(e) => setPwd(e.target.value)}
+							value={pwd}
+							required
+						/>
+						<button>Sign In</button>
+						<p>
+							Need an account?
+							<br />
+							<span className='line'>Sign Up</span>
+						</p>
+					</form>
+				</section>
+			)}
+		</>
 	)
 }
 
