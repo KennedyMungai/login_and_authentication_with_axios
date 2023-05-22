e3b07843fc755d4dffb2874821b3d504f44f2bfa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-22 12:48:05 +0300

    Defined the user, pwd and errMsg states

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,11 @@ import { useEffect, useRef, useState } from 'react'
 
 const Login = () => {
     const userRef = useRef(null)
-	const errRef = useRef(null)
+    const errRef = useRef(null)
+    
+    const [user, setUser] = useState('')
+	const [pwd, setPwd] = useState('')
+	const [errMsg, setErrMsg] = useState('')
 
 	return <div>Login</div>
 }
